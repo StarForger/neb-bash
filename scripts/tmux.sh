195552fc65b1e3b0ce83c46d3906052bde874cbe
Frederick Shier <fred.shier@gmail.com>
2021-02-16 14:34:36 +0000

    fix function names and add url suffix check

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-
-tmux_status() {
+function tmux_status() {
     local -r session_name="${1}"    
     # $(tmux list-sessions -F "#{session_name}" 2>/dev/null | grep -Ecx "^${session_name}" || true)  
     tmux has-session -t "${session_name}"  
--- scripts/url.sh
+++ scripts/url.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
 # return true if string starts with http(s):// and can be considered a url
-url_check() {
+function url_check() {
   local -r value="${1}"
 
   [[ ${value:0:8} == "https://" || ${value:0:7} == "http://" ]]    
 }
 
+# return true if url points to file with suffix
+function url_valid_file() {
+  local -r suffix=${1}
+  local -r url=${2}
+
+  [[ "$url" == http*://*.${suffix} || "$url" == http*://*.${suffix}\?* ]]
+}
+
 # resolve url to final effective one
-url_resolve() {
+function url_resolve() {
   local -r url="${1}"
   local -r resolved_url=$(curl -Ls -o /dev/null -w %{url_effective} "$url")
   
@@ -16,7 +24,7 @@ url_resolve() {
 }
 
 # get filename from url
-url_filename() {
+function url_filename() {
   local -r url="${1}"
   local -r strippedOfQuery="${url%\?*}"
   basename "$strippedOfQuery"
