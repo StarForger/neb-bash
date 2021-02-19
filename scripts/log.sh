22fd4b2b5e59a11f23358b31d9c946b244d744a5
Frederick Shier <fred.shier@gmail.com>
2021-02-19 08:43:22 +0000

    log and tmux changes

--- scripts/log.sh
+++ scripts/log.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 
+. "./array.sh"
+
 # Log the given message at the given level. All logs are written to stderr with a timestamp.
 function log {
-  local -r level="$1"
-  local -r message="$2"
-  local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  if [[ "$#" -lt 1 ]]; then
+    log "ERROR" "log() missing level"
+    return 2
+  fi 
+  local -r level="${1}"; shift
+  local -r valid_levels=(
+    "INFO"
+    "WARN"
+    "ERROR"
+  )
+  if ! array_contains "${level}" "${valid_levels}"; then
+    log "ERROR" "Log level ${level} is not a valid level."
+    return 2
+  fi  
   local -r script_name="$(basename "$0")"
-  >&2 echo -e "${timestamp} [${level}] [$script_name] ${message}"
-}
+  # local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  (
+    ([[ "$#" -ge 1 ]] && <<<"${*}" cat -) || cat -
+  ) | awk -v level="${level}" '{ printf("%s [%s] %s\n", strftime("%FT%T%z"), level, $0); fflush(); }'
+} >&2
 
 # Log the given message at INFO level. All logs are written to stderr with a timestamp.
 function log_info {
   local -r message="$1"
-  log "INFO" "$message"
+
+  log "INFO" "$message" <<< -
 }
 
 # Log the given message at WARN level. All logs are written to stderr with a timestamp.
--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-function tmux_status() {
+function tmux_session_running() {
     local -r session_name="${1}"    
-    # $(tmux list-sessions -F "#{session_name}" 2>/dev/null | grep -Ecx "^${session_name}" || true)  
-    tmux has-session -t "${session_name}"  
+    local -r sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | grep -Ecx "^${session_name}" || true)  
+    # tmux has-session -t "${session_name}" 2>/dev/null
+    [[ "${sessions}" -gt "0" ]]        
 }
